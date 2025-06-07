42e77ef99625cfd9bf20b4bb4fa36ff13dc593f4
ChaturVasireddy <chaturvasireddy@gmail.com>
2025-06-08 00:46:25 +0530

    added deadzone for joystick test file

--- arduino_joystick_test/arduino_joystick_test.ino
+++ arduino_joystick_test/arduino_joystick_test.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+ int x,y;
+
 void setup()
 {
   Serial.begin(9600);
@@ -7,9 +9,14 @@ void setup()
 
 void loop()
 {
-  Serial.print(analogRead(A2));
+  if (analogRead(A2)>=484.5 && analogRead(A2)<=539.5) x=512;    //deadzone of 27.5 (obtained from 255*1.9=484.5)
+  else x=analogRead(A2);
+  if (analogRead(A3)>=484.5 && analogRead(A3)<=539.5) y=512;
+  else y=analogRead(A3);
+
+  Serial.print(x);
   Serial.print("    ");
-  Serial.print(analogRead(A3));
+  Serial.print(y);
   Serial.print("    ");
   Serial.println(analogRead(A4));
   delay(100);
