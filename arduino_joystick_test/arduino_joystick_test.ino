db31329499a4310d7e6822071d2911d996331080
ChaturVasireddy <chaturvasireddy@gmail.com>
2025-06-08 10:45:47 +0530

    bug fix

--- arduino_joystick_test/arduino_joystick_test.ino
+++ arduino_joystick_test/arduino_joystick_test.ino
@@ -9,12 +9,12 @@ void setup()
 
 void loop()
 {
-  xr=analogread(A2);
+  xr=analogRead(A2);
   if (xr>=484.5 && xr<=539.5) x=484.5;    //deadzone of 27.5 (obtained from 255*1.9=484.5) => ranges from 0 to 969
   else if (xr<484.5) x=(xr);
   else if (xr>539.5) x=(xr-55.0);
 
-  yr=analogread(A3);
+  yr=analogRead(A3);
   if (yr>=484.5 && yr<=539.5) y=484.5;
   else if (yr<484.5) y=(yr);
   else if (yr>539.5) y=(yr-55.0);
